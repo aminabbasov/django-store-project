3db7e661de3a971d63332c8aca1efd744b1b083c
aminabbasov <aminabbasov@protonmail.com>
2023-07-02 15:05:35 +0400

    Change db to postgres

--- src/app/conf/db.py
+++ src/app/conf/db.py
@@ -6,9 +6,12 @@ from app.conf.environ import env
 
 DATABASES = {
     # read os.environ["DATABASE_URL"] and raises ImproperlyConfigured exception if not found
-    "default": env.db(),
+    "default": env.db(), 
 }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if not env("DEBUG", cast=bool):
+    DATABASES["default"]["CONN_MAX_AGE"] = 600
